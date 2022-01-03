7fd92eead8fd9c06e6c75f62c55a94b4b5af2a92
Amarjitkaur <amarjit.tws@tekkiwebsolutions.com>
2022-01-03 14:46:47 +0530

    static_merge

--- 1.py
+++ 1.py
@@ -1,14 +1,16 @@
-dic = dict()
-def funtion():
-    for i in range(2):
-        keys = input('Enter the name of Employee:')
-        values = int(input('Enter the Phone number of Employee:'))
-        dic[keys] = values
-    print(dic)
-    search = input('Enter the  Name of Employee you want to be search phone number: ')
-    if search in dic:
-        print('Employee phone number:',dic[search])
-    else:
-        print('Does not exist!!')
+dic = {'a':123,'b':4454}
+# for i in range(2):
+#         keys = input('Enter the name of Employee:')
+#         values = int(input('Enter the Phone number of Employee:'))
+#         dic[keys] = values
+# print(dic)
+# search = input('Enter the  Name of Employee you want to be search phone number: ')
 
-funtion()
+def funtion(search):
+    if search in dic:
+        return dic[search]
+    else:
+        return 'Does not exist!!'
+
+a = funtion(input('Enter the  Name of Employee you want to be search phone number: '))
+print(a)
